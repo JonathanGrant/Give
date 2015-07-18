567fd0681c28015c67c531a3d688118605b628b9
shiga yuichi <yuichi.shiga@gmail.com>
2015-07-17 22:09:37 -0700

    added briding header

---
+++ Give/ObjectiveCTestObj.m
@@ -0,0 +1,9 @@
+//
+//  ObjectiveCTestObj.m
+//  Give
+//
+//  Created by shiga yuichi on 2015/07/17.
+//  Copyright (c) 2015年 Jonathan Grant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
